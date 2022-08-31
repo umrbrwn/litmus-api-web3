3b751e695fba6740b57b51a5f6f2f2916754bc4d
umar <37788956+umrbrwn@users.noreply.github.com>
2022-08-31 11:46:21 +0500

    Migration to web3.0: 1. Removed users model and password based auth 2. Now using polkadot to verify signature based on address/username

--- src/models/User.js
+++ src/models/User.js
@@ -1,39 +1,20 @@
 import Joi from 'joi';
-import { dbcontext } from '../modules/db.js';
+
+const addressMatcher = (value, helpers) => {
+  const [payload] = helpers.state.ancestors;
+  const pattern = new RegExp(`(?<![\\w\\d])${payload.username}(?![\\w\\d])`)
+  if (!pattern.test(value)) {
+    return helpers.message('address in the "message" should match "username".');
+  }
+  return value;
+};
 
 const validations = {
-  newUser: Joi.object({
-    username: Joi.string().alphanum().min(3).max(100).required(),
-    password: Joi.string().min(6).max(65).required()
+  user: Joi.object({
+    username: Joi.string().alphanum().max(100).required(),
+    signature: Joi.string().max(1000).required(),
+    message: Joi.string().required().custom(addressMatcher),
   })
 };
 
-class User {
-  static findByUsername(username) {
-    return dbcontext.users.findFirst({ where: { username } });
-  }
-
-  static createUser(user) {
-    return dbcontext.users.create({
-      data: {
-        username: user.username,
-        passwordHash: user.passwordHash,
-        salt: user.salt,
-        failedLoginCount: 0
-      }
-    });
-  }
-
-  // For now we are only incrementing, we don't really need this
-  // in real world scenario this should have a max failed count limit
-  // and after that user account should be locked for a time period
-  static async countLoginFailed(username) {
-    const user = await User.findByUsername(username);
-    return dbcontext.users.update({
-      where: { userId: user.userId },
-      data: { failedLoginCount: user.failedLoginCount + 1 }
-    })
-  }
-}
-
-export { User as default, validations };
+export { validations };
